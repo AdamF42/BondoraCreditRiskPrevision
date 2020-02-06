944044a0a24c2d4c1035c94a322c75f2f4a7036e
Adam <adamofapohunda@gmail.com>
2020-02-06 16:35:21 +0100

    fix

--- src/main/scala/BondoraCreditDefault.scala
+++ src/main/scala/BondoraCreditDefault.scala
@@ -1,8 +1,18 @@
-
-
 import org.apache.spark.sql.SparkSession
 
 object BondoraCreditDefault {
+
+  val features = Seq("Age",
+    "NewCreditCustomer",
+    "Country",
+    "AppliedAmount",
+    "Interest",
+    "LoanDuration",
+    "UseOfLoan",
+    "MaritalStatus",
+    "EmploymentStatus",
+    "IncomeTotal",
+    "Status")
 
   def setupLogging(): Unit = {
     import org.apache.log4j.{Level, Logger}
@@ -18,27 +28,15 @@ object BondoraCreditDefault {
       .master("local[*]")
       .getOrCreate()
 
-    //import spark.implicits._
-
-
     setupLogging()
 
     val df = spark.read.format("csv")
       .option("header", value = true)
       .load("../LoanData.csv")
 
-    val endedLoans = df.select("Age",
-      "NewCreditCustomer",
-      "Country",
-      "AppliedAmount",
-      "Interest",
-      "LoanDuration",
-      "UseOfLoan",
-      "MaritalStatus",
-      "EmploymentStatus",
-      "IncomeTotal",
-      "Status")
+    val endedLoans = df.select(features.head,features.tail:_*)
       .where(df.col("ContractEndDate").isNotNull)
 
   }
+
 }
