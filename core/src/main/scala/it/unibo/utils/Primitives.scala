aac6b2bd899fd8c6e4816ea49f16b5b0d739fc43
Adamo Fapohunda <adamofapohunda@gmail.com>
2020-03-15 22:33:48 +0100

    Added utility functions to make benchmarks

---
+++ core/src/main/scala/it/unibo/utils/Primitives.scala
@@ -0,0 +1,64 @@
+package it.unibo.utils
+
+import scala.concurrent.forkjoin._
+import scala.util.DynamicVariable
+
+object Primitives {
+
+  val forkJoinPool = new ForkJoinPool
+  val scheduler =
+    new DynamicVariable[TaskScheduler](new DefaultTaskScheduler)
+
+  def time[R](block: => R): R = {
+    val t0 = System.nanoTime()
+    val result = block // call-by-name
+    val t1 = System.nanoTime()
+    println("Elapsed time: " + (t1 - t0) / 1000000 + "ms")
+    result
+  }
+
+  def parallel[A, B](taskA: => A, taskB: => B): (A, B) = {
+    scheduler.value.parallel(taskA, taskB)
+  }
+
+  def parallel[A, B, C, D](taskA: => A, taskB: => B, taskC: => C, taskD: => D): (A, B, C, D) = {
+    val ta = task {
+      taskA
+    }
+    val tb = task {
+      taskB
+    }
+    val tc = task {
+      taskC
+    }
+    val td = taskD
+    (ta.join(), tb.join(), tc.join(), td)
+  }
+
+  def task[T](body: => T): ForkJoinTask[T] = {
+    scheduler.value.schedule(body)
+  }
+
+  abstract class TaskScheduler {
+    def schedule[T](body: => T): ForkJoinTask[T]
+
+    def parallel[A, B](taskA: => A, taskB: => B): (A, B) = {
+      val right = task {
+        taskB
+      }
+      val left = taskA
+      (left, right.join())
+    }
+  }
+
+  class DefaultTaskScheduler extends TaskScheduler {
+    def schedule[T](body: => T): ForkJoinTask[T] = {
+      val t = new RecursiveTask[T] {
+        def compute: T = body
+      }
+      forkJoinPool.execute(t)
+      t
+    }
+  }
+
+}
